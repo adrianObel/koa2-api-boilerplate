35fc841fc72e3a790e9f78be81f840f2f376815b
Adrian Obelmejias <obeladrian@gmail.com>
2016-02-11 17:55:16 -0500

    Add password hashing

--- src/models/users.js
+++ src/models/users.js
@@ -1,10 +1,43 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
 
 const User = new mongoose.Schema({
   type: { type: String, default: 'User' },
   name: { type: String },
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  salt: { type: String, required: true }
 })
+
+User.pre('save', function(next) {
+  const user = this
+
+  if(!user.isModified('password')) {
+    return next()
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if(err) { return next(err) }
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if(err) { return next(err) }
+
+      user.password = hash
+      next()
+    })
+  })
+})
+
+User.methods.validatePassword = function(password) {
+  const user = this
+
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, user.password, (err, isMatch) => {
+      if(err) { return reject(err) }
+
+      resolve(isMatch)
+    });
+  })
+};
 
 export default mongoose.model('user', User)
