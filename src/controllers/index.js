d64742f36f8d4d4115e00e6864251431d962fd4b
Adrian Obelmejias <obeladrian@gmail.com>
2016-02-11 19:27:49 -0500

    add auth routes

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,11 @@
 import users from './users'
+import auth from './auth'
 
 exports = module.exports = function Controllers(app) {
   app
+    .use(auth.routes())
+    .use(auth.allowedMethods())
+
     .use(users.routes())
     .use(users.allowedMethods())
 }
