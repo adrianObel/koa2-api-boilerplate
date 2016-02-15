e87f6264da6a6923172fd3668a39f247db2168eb
Adrian Obelmejias <obeladrian@gmail.com>
2016-02-15 18:15:08 -0500

    Add tests for /user/:id

--- test/users.spec.js
+++ test/users.spec.js
@@ -52,8 +52,38 @@ describe('Users', () => {
     })
 
     it('should fetch all users', (done) => {
+      const { token } = context
       request
-        .get(`/users?token=${context.token}`)
+        .get(`/users?token=${token}`)
+        .set('Accept', 'application/json')
+        .expect(200, done)
+    })
+  })
+
+  describe('GET /users/:id', () => {
+    it('should not fetch user if token is invalid', (done) => {
+      request
+        .get('/users/1?token=1')
+        .set('Accept', 'application/json')
+        .expect(401, done)
+    })
+
+    it('should throw 404 if user doesn\'t exist', (done) => {
+      const { token } = context
+      request
+        .get(`/users/1?token=${token}`)
+        .set('Accept', 'application/json')
+        .expect(404, done)
+    })
+
+    it('should fetch user', (done) => {
+      const {
+        user: { _id },
+        token
+      } = context
+
+      request
+        .get(`/users/${_id}?token=${token}`)
         .set('Accept', 'application/json')
         .expect(200, done)
     })
