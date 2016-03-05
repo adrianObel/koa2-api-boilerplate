184451f9fc9d13bbee153890490b47f9b2a9044e
Adrian Obelmejias <obeladrian@gmail.com>
2016-03-05 13:38:08 -0500

    Create router for `/users`

---
+++ src/modules/users/router.js
@@ -0,0 +1,48 @@
+import { createUser } from './controller'
+
+export default {
+  base: '/users',
+
+/**
+ * @api {post} /users Create a new user
+ * @apiPermission
+ * @apiVersion 1.0.0
+ * @apiName CreateUser
+ * @apiGroup Users
+ *
+ * @apiExample Example usage:
+ * curl -H "Content-Type: application/json" -X POST -d '{ "user": { "username": "johndoe", "password": "secretpasas" } }' localhost:5000/users
+ *
+ * @apiParam {Object} user          User object (required)
+ * @apiParam {String} user.username Username.
+ * @apiParam {String} user.password Password.
+ *
+ * @apiSuccess {Object}   users           User object
+ * @apiSuccess {ObjectId} users._id       User id
+ * @apiSuccess {String}   users.name      User name
+ * @apiSuccess {String}   users.username  User username
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "user": {
+ *          "_id": "56bd1da600a526986cf65c80"
+ *          "name": "John Doe"
+ *          "username": "johndoe"
+ *       }
+ *     }
+ *
+ * @apiError UnprocessableEntity Missing required parameters
+ *
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 422 Unprocessable Entity
+ *     {
+ *       "status": 422,
+ *       "error": "Unprocessable Entity"
+ *     }
+ */
+  '/': {
+    method: 'POST',
+    controller: createUser
+  }
+}
