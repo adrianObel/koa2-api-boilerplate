cb8f26fdc9dcc561142ebbeafc735f9089d83bf4
Adrian Obelmejias <obeladrian@gmail.com>
2016-03-04 20:02:38 -0500

    Split environment specific config into separate files

--- config/config.js
+++ config/config.js
@@ -1,24 +1,6 @@
+import common from './env/common'
+
 const env = process.env.NODE_ENV || 'development'
+const config = require(`./env/${env}`).default
 
-const config = {
-  common: {
-    port: process.env.PORT || 5000
-  },
-  test: {
-    session: 'secret-boilerplate-token',
-    token: 'secret-jwt-token',
-    database: 'mongodb://localhost:27017/test'
-  },
-  development: {
-    session: 'secret-boilerplate-token',
-    token: 'secret-jwt-token',
-    database: 'mongodb://localhost:27017/koa2-boilerplate'
-  },
-  production: {
-    session: 'secret-boilerplate-token',
-    token: 'secret-jwt-token',
-    database: 'mongodb://localhost:27017/koa2-boilerplate'
-  }
-}
-
-export default Object.assign({}, config.common, config[env])
+export default Object.assign({}, common, config)
---
+++ config/env/production.js
@@ -0,0 +1,5 @@
+export default {
+  session: 'secret-boilerplate-token',
+  token: 'secret-jwt-token',
+  database: 'mongodb://localhost:27017/koa2-boilerplate-prod'
+}
