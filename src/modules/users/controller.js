2d8e171d076328c01f81b6a24714c28dbd3e8cf0
Adrian Obelmejias <obeladrian@gmail.com>
2016-03-16 20:18:27 -0400

    Add PUT /users/:id

--- src/modules/users/controller.js
+++ src/modules/users/controller.js
@@ -22,12 +22,12 @@ export async function createUser(ctx) {
   }
 }
 
-export async function getUsers (ctx) {
+export async function getUsers(ctx) {
   const users = await User.find({}, '-password -salt')
   ctx.body = users
 }
 
-export async function getUser (ctx) {
+export async function getUser(ctx) {
   try {
     const user = await User.findById(ctx.params.id, '-password -salt')
     if (!user) {
@@ -35,6 +35,28 @@ export async function getUser (ctx) {
     }
 
     ctx.body = user
+  } catch (err) {
+    if (err === 404 || err.name === 'CastError') {
+      ctx.throw(404)
+    }
+
+    ctx.throw(500)
+  }
+}
+
+export async function updateUser(ctx) {
+  try {
+    const user = await User.findById(ctx.params.id, '-password -salt')
+    if (!user) {
+      ctx.throw(404)
+    }
+
+    Object.assign(user, ctx.request.body.user)
+
+    await user.save()
+    ctx.body = {
+      user
+    }
   } catch (err) {
     if (err === 404 || err.name === 'CastError') {
       ctx.throw(404)
--- src/modules/users/router.js
+++ src/modules/users/router.js
@@ -1,126 +1,41 @@
 import { ensureUser } from '../../middleware/validators'
-import {
-  createUser,
-  getUsers,
-  getUser
-} from './controller'
+import * as user from './controller'
 
 export default {
   base: '/users',
 
-  /**
-   * @api {post} /users Create a new user
-   * @apiPermission
-   * @apiVersion 1.0.0
-   * @apiName CreateUser
-   * @apiGroup Users
-   *
-   * @apiExample Example usage:
-   * curl -H "Content-Type: application/json" -X POST -d '{ "user": { "username": "johndoe", "password": "secretpasas" } }' localhost:5000/users
-   *
-   * @apiParam {Object} user          User object (required)
-   * @apiParam {String} user.username Username.
-   * @apiParam {String} user.password Password.
-   *
-   * @apiSuccess {Object}   users           User object
-   * @apiSuccess {ObjectId} users._id       User id
-   * @apiSuccess {String}   users.name      User name
-   * @apiSuccess {String}   users.username  User username
-   *
-   * @apiSuccessExample {json} Success-Response:
-   *     HTTP/1.1 200 OK
-   *     {
-   *       "user": {
-   *          "_id": "56bd1da600a526986cf65c80"
-   *          "name": "John Doe"
-   *          "username": "johndoe"
-   *       }
-   *     }
-   *
-   * @apiError UnprocessableEntity Missing required parameters
-   *
-   * @apiErrorExample {json} Error-Response:
-   *     HTTP/1.1 422 Unprocessable Entity
-   *     {
-   *       "status": 422,
-   *       "error": "Unprocessable Entity"
-   *     }
-   */
   'Create new users': {
     method: 'POST',
     route: '/',
-    controller: createUser
-  },
-
-  /**
-   * @api {get} /users Get all users
-   * @apiPermission user
-   * @apiVersion 1.0.0
-   * @apiName GetUsers
-   * @apiGroup Users
-   *
-   * @apiExample Example usage:
-   * curl -H "Content-Type: application/json" -X GET localhost:5000/users
-   *
-   * @apiSuccess {Object[]} users           Array of user objects
-   * @apiSuccess {ObjectId} users._id       User id
-   * @apiSuccess {String}   users.name      User name
-   * @apiSuccess {String}   users.username  User username
-   *
-   * @apiSuccessExample {json} Success-Response:
-   *     HTTP/1.1 200 OK
-   *     {
-   *       "users": [{
-   *          "_id": "56bd1da600a526986cf65c80"
-   *          "name": "John Doe"
-   *          "username": "johndoe"
-   *       }]
-   *     }
-   *
-   * @apiUse TokenError
-   */
-  'Get all users': {
-    method: 'GET',
-    route: '/',
-    controller: getUsers,
-    middleware: [
-      ensureUser
+    handlers: [
+      user.createUser
     ]
   },
 
-  /**
-   * @api {get} /users/:id Get user by id
-   * @apiPermission user
-   * @apiVersion 1.0.0
-   * @apiName GetUser
-   * @apiGroup Users
-   *
-   * @apiExample Example usage:
-   * curl -H "Content-Type: application/json" -X GET localhost:5000/users/56bd1da600a526986cf65c80
-   *
-   * @apiSuccess {Object}   users           User object
-   * @apiSuccess {ObjectId} users._id       User id
-   * @apiSuccess {String}   users.name      User name
-   * @apiSuccess {String}   users.username  User username
-   *
-   * @apiSuccessExample {json} Success-Response:
-   *     HTTP/1.1 200 OK
-   *     {
-   *       "user": {
-   *          "_id": "56bd1da600a526986cf65c80"
-   *          "name": "John Doe"
-   *          "username": "johndoe"
-   *       }
-   *     }
-   *
-   * @apiUse TokenError
-   */
+  'Get all users': {
+    method: 'GET',
+    route: '/',
+    handlers: [
+      ensureUser,
+      user.getUsers
+    ]
+  },
+
   'Get a single user': {
     method: 'GET',
     route: '/:id',
-    controller: getUser,
-    middleware: [
-      ensureUser
+    handlers: [
+      ensureUser,
+      user.getUser
+    ]
+  },
+
+  'Update user': {
+    method: 'PUT',
+    route: '/:id',
+    handlers: [
+      ensureUser,
+      user.updateUser
     ]
   }
 }
