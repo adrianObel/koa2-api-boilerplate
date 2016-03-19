f29e6e04325b82166da75dec86f8c704c3191068
Adrian Obelmejias <obeladrian@gmail.com>
2016-03-19 11:26:39 -0400

    Add docs

--- src/modules/users/controller.js
+++ src/modules/users/controller.js
@@ -1,5 +1,43 @@
 import User from '../../models/users'
 
+/**
+ * @api {post} /users Create a new user
+ * @apiPermission
+ * @apiVersion 1.0.0
+ * @apiName CreateUser
+ * @apiGroup Users
+ *
+ * @apiExample Example usage:
+ * curl -H "Content-Type: application/json" -X POST -d '{ "user": { "username": "johndoe", "password": "secretpasas" } }' localhost:5000/users
+ *
+ * @apiParam {Object} user          User object (required)
+ * @apiParam {String} user.username Username.
+ * @apiParam {String} user.password Password.
+ *
+ * @apiSuccess {Object}   users           User object
+ * @apiSuccess {ObjectId} users._id       User id
+ * @apiSuccess {String}   users.name      User name
+ * @apiSuccess {String}   users.username  User username
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "user": {
+ *          "_id": "56bd1da600a526986cf65c80"
+ *          "name": "John Doe"
+ *          "username": "johndoe"
+ *       }
+ *     }
+ *
+ * @apiError UnprocessableEntity Missing required parameters
+ *
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 422 Unprocessable Entity
+ *     {
+ *       "status": 422,
+ *       "error": "Unprocessable Entity"
+ *     }
+ */
 export async function createUser(ctx) {
   const user = new User(ctx.request.body.user)
   try {
@@ -20,11 +58,65 @@ export async function createUser(ctx) {
   }
 }
 
+/**
+ * @api {get} /users Get all users
+ * @apiPermission user
+ * @apiVersion 1.0.0
+ * @apiName GetUsers
+ * @apiGroup Users
+ *
+ * @apiExample Example usage:
+ * curl -H "Content-Type: application/json" -X GET localhost:5000/users
+ *
+ * @apiSuccess {Object[]} users           Array of user objects
+ * @apiSuccess {ObjectId} users._id       User id
+ * @apiSuccess {String}   users.name      User name
+ * @apiSuccess {String}   users.username  User username
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "users": [{
+ *          "_id": "56bd1da600a526986cf65c80"
+ *          "name": "John Doe"
+ *          "username": "johndoe"
+ *       }]
+ *     }
+ *
+ * @apiUse TokenError
+ */
 export async function getUsers(ctx) {
   const users = await User.find({}, '-password -salt')
   ctx.body = { users }
 }
 
+/**
+ * @api {get} /users/:id Get user by id
+ * @apiPermission user
+ * @apiVersion 1.0.0
+ * @apiName GetUser
+ * @apiGroup Users
+ *
+ * @apiExample Example usage:
+ * curl -H "Content-Type: application/json" -X GET localhost:5000/users/56bd1da600a526986cf65c80
+ *
+ * @apiSuccess {Object}   users           User object
+ * @apiSuccess {ObjectId} users._id       User id
+ * @apiSuccess {String}   users.name      User name
+ * @apiSuccess {String}   users.username  User username
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "user": {
+ *          "_id": "56bd1da600a526986cf65c80"
+ *          "name": "John Doe"
+ *          "username": "johndoe"
+ *       }
+ *     }
+ *
+ * @apiUse TokenError
+ */
 export async function getUser(ctx, next) {
   try {
     const user = await User.findById(ctx.params.id, '-password -salt')
@@ -46,6 +138,46 @@ export async function getUser(ctx, next) {
   next()
 }
 
+/**
+ * @api {put} /users/:id Update a user
+ * @apiPermission
+ * @apiVersion 1.0.0
+ * @apiName UpdateUser
+ * @apiGroup Users
+ *
+ * @apiExample Example usage:
+ * curl -H "Content-Type: application/json" -X PUT -d '{ "user": { "name": "Cool new Name" } }' localhost:5000/users/56bd1da600a526986cf65c80
+ *
+ * @apiParam {Object} user          User object (required)
+ * @apiParam {String} user.name     Name.
+ * @apiParam {String} user.username Username.
+ *
+ * @apiSuccess {Object}   users           User object
+ * @apiSuccess {ObjectId} users._id       User id
+ * @apiSuccess {String}   users.name      Updated name
+ * @apiSuccess {String}   users.username  Updated username
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "user": {
+ *          "_id": "56bd1da600a526986cf65c80"
+ *          "name": "Cool new name"
+ *          "username": "johndoe"
+ *       }
+ *     }
+ *
+ * @apiError UnprocessableEntity Missing required parameters
+ *
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 422 Unprocessable Entity
+ *     {
+ *       "status": 422,
+ *       "error": "Unprocessable Entity"
+ *     }
+ *
+ * @apiUse TokenError
+ */
 export async function updateUser(ctx) {
   const user = ctx.body.user
 
@@ -57,6 +189,27 @@ export async function updateUser(ctx) {
     user
   }
 }
+
+/**
+ * @api {delete} /users/:id Delete a user
+ * @apiPermission
+ * @apiVersion 1.0.0
+ * @apiName DeleteUser
+ * @apiGroup Users
+ *
+ * @apiExample Example usage:
+ * curl -H "Content-Type: application/json" -X DELETE localhost:5000/users/56bd1da600a526986cf65c80
+ *
+ * @apiSuccess {StatusCode} 200
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "success": true
+ *     }
+ *
+ * @apiUse TokenError
+ */
 
 export async function deleteUser(ctx) {
   const user = ctx.body.user
