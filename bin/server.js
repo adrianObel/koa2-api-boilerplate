bb6fe2eccc6567530a1cf2af44093bbf272ec50f
Adrian Obelmejias <obeladrian@gmail.com>
2016-11-05 13:40:27 -0400

    Remove sessions as API should be stateless

--- bin/server.js
+++ bin/server.js
@@ -1,9 +1,7 @@
 import Koa from 'koa'
 import bodyParser from 'koa-bodyparser'
-import convert from 'koa-convert'
 import logger from 'koa-logger'
 import mongoose from 'mongoose'
-import session from 'koa-generic-session'
 import passport from 'koa-passport'
 import mount from 'koa-mount'
 import serve from 'koa-static'
@@ -17,16 +15,14 @@ app.keys = [config.session]
 mongoose.Promise = global.Promise
 mongoose.connect(config.database)
 
-app.use(convert(logger()))
+app.use(logger())
 app.use(bodyParser())
-app.use(session())
 app.use(errorMiddleware())
 
-app.use(convert(mount('/docs', serve(`${process.cwd()}/docs`))))
+app.use(mount('/docs', serve(`${process.cwd()}/docs`)))
 
-require('../config/passport')
+require('../lib/passport')
 app.use(passport.initialize())
-app.use(passport.session())
 
 const modules = require('../src/modules')
 modules(app)
