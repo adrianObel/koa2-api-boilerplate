f94ecd75b042587a92db2d4b8f58e5d0c1780e25
Adrian Obelmejias <obeladrian@gmail.com>
2017-11-25 10:54:33 -0500

    Add timestamps to user model

--- src/models/users.js
+++ src/models/users.js
@@ -1,55 +1,11 @@
-import mongoose from 'mongoose'
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import config from 'config'
+import bookshelf from 'db'
 
-const User = new mongoose.Schema({
-  name: { type: String },
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+const User = bookshelf.Model.extend({
+  tableName: 'users',
+  hasTimestamps: true,
+
+  // stripped from query
+  hidden: ['password']
 })
 
-User.pre('save', function preSave (next) {
-  const user = this
-
-  if (!user.isModified('password')) {
-    return next()
-  }
-
-  new Promise((resolve, reject) => {
-    bcrypt.genSalt(10, (err, salt) => {
-      if (err) { return reject(err) }
-      resolve(salt)
-    })
-  })
-  .then(salt => {
-    bcrypt.hash(user.password, salt, (err, hash) => {
-      if (err) { throw new Error(err) }
-
-      user.password = hash
-
-      next(null)
-    })
-  })
-  .catch(err => next(err))
-})
-
-User.methods.validatePassword = function validatePassword (password) {
-  const user = this
-
-  return new Promise((resolve, reject) => {
-    bcrypt.compare(password, user.password, (err, isMatch) => {
-      if (err) { return reject(err) }
-
-      resolve(isMatch)
-    })
-  })
-}
-
-User.methods.generateToken = function generateToken () {
-  const user = this
-
-  return jwt.sign({ id: user.id }, config.token)
-}
-
-export default mongoose.model('user', User)
+export default User
