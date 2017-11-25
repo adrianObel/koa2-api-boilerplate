ab909964ea629e91c0c1f3a9356b4f2356440126
Adrian Obelmejias <obeladrian@gmail.com>
2017-11-25 01:15:24 -0500

    Add postgres image

---
+++ docker/init.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE koa-boilerplate;
+CREATE DATABASE koa-boilerplate-test;
+
+GRANT ALL PRIVILEGES ON DATABASE koa-boilerplate TO root;
+GRANT ALL PRIVILEGES ON DATABASE koa-boilerplate-test TO root;
