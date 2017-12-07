5bfbcb3b4fb2bb1ec696c28dfadc8083b528aff2
Adrian Obelmejias <obeladrian@gmail.com>
2017-12-07 12:42:32 -0500

    Clean database between integration tests

--- src/modules/users/__test__/module.spec.js
+++ src/modules/users/__test__/module.spec.js
@@ -1,11 +1,18 @@
 import app from '../../../../bin/server'
 import supertest from 'supertest'
 import faker from 'faker'
+import knexCleaner from 'knex-cleaner'
+import db from 'db'
 
 const request = supertest.agent(app.listen())
 
 describe('(Module) users', () => {
+  beforeEach(() => {
+    return knexCleaner.clean(db.knex)
+  })
+
   describe('POST /users', async () => {
+
     it('should sign up', async () => {
       const data = {
         user: {
@@ -26,6 +33,20 @@ describe('(Module) users', () => {
       expect(user).toBeTruthy()
       expect(user.name).toBe(data.user.name)
       expect(user.email).toBe(data.user.email)
+    })
+  })
+
+  describe('GET /users', async () => {
+    it('should list all users', async () => {
+      const res = await request
+        .get('/users')
+        .set('Accept', 'application/json')
+        .expect(200)
+
+      const { users } = res.body
+
+      expect(users).toBeTruthy()
+      console.log(users)
     })
   })
 })
