23c2d55465996f2ac61418dc88c8985675e86b22
Adrian Obelmejias <obeladrian@gmail.com>
2017-12-07 12:42:19 -0500

    Add migration fix for unique constraints

--- migrations/20171125014420_users.js
+++ migrations/20171125014420_users.js
@@ -1,6 +1,14 @@
 
 exports.up = function (knex, Promise) {
-  return knex.schema.createTableIfNotExists('users', function (table) {
+  // Workaround for https://github.com/tgriesser/knex/issues/1303
+  //
+  // createTableIfNotExists invokes the callback function
+  // even when it should't actually run the migration
+  // this causes migrations to fail by trying to duplicate constraints
+  const exists = knex.schema.hasTable('users')
+  if (exists) { return Promise.resolve() }
+
+  return knex.schema.createTable('users', function (table) {
     table.increments('id').primary()
     table.string('name')
     table.string('email').unique()
