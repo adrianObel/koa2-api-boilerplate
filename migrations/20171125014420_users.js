004ebc2cfa5ad929206192c2864c2f72203009cf
Adrian Obelmejias <obeladrian@gmail.com>
2017-12-07 17:40:29 -0500

    hasTable returns a promise...

--- migrations/20171125014420_users.js
+++ migrations/20171125014420_users.js
@@ -1,23 +1,28 @@
+const TABLE_NAME = 'users'
 
 exports.up = function (knex, Promise) {
   // Workaround for https://github.com/tgriesser/knex/issues/1303
-  //
   // createTableIfNotExists invokes the callback function
   // even when it should't actually run the migration
   // this causes migrations to fail by trying to duplicate constraints
-  const exists = knex.schema.hasTable('users')
-  if (exists) { return Promise.resolve() }
+  return knex.schema.hasTable(TABLE_NAME)
+    .then(exists => {
+      if (exists) {
+        console.log(`===${TABLE_NAME} table found. Skipping===`)
+        return
+      }
 
-  return knex.schema.createTable('users', function (table) {
-    table.increments('id').primary()
-    table.string('name')
-    table.string('email').unique()
-    table.boolean('admin').defaultTo(false)
-    table.string('password')
-    table.timestamps()
-  })
+      return knex.schema.createTable(TABLE_NAME, function (table) {
+        table.increments('id').primary()
+        table.string('name')
+        table.string('email').unique()
+        table.boolean('admin').defaultTo(false)
+        table.string('password')
+        table.timestamps()
+      })
+    })
 }
 
 exports.down = function (knex, Promise) {
-  return knex.schema.dropTable('users')
+  return knex.schema.dropTable(TABLE_NAME)
 }
