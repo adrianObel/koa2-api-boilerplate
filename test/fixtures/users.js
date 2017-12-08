a5ff819ac4eeefe9cac192a634307388b7389159
Adrian Obelmejias <obeladrian@gmail.com>
2017-12-08 00:51:14 -0500

    Fix default attrs

--- test/fixtures/users.js
+++ test/fixtures/users.js
@@ -5,11 +5,11 @@ import faker from 'faker'
  * Generate user fixture
  */
 export function createUser (attrs = {}) {
-  Object.assign(attrs, {
+  attrs = Object.assign({
     name: faker.name.findName(),
     email: faker.internet.email(),
     password: faker.internet.password()
-  })
+  }, attrs)
 
   const user = new User(attrs)
 
