121fc97271fdee93f91707525d4e245437b8a9f6
Adrian Obelmejias <obeladrian@gmail.com>
2017-12-07 23:47:36 -0500

    Generate password hash in pre-save hook

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
-import { Model } from 'db'
+import bcrypt from 'bcryptjs'
 import Joi from 'joi'
+import { Model } from 'db'
 
 const User = Model.extend({
   tableName: 'users',
@@ -11,7 +12,18 @@ const User = Model.extend({
   },
 
   // stripped from query
-  hidden: ['password']
+  hidden: ['password'],
+
+  initialize () {
+    this.on('saving', async () => {
+      const hash = await bcrypt.hash(this.get('password'), 13)
+      this.set('password', hash)
+    })
+  },
+
+  verifyPassword (password) {
+    return bcrypt.compare(password, this.get('password'))
+  }
 })
 
 export default User
